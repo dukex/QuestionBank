8cb0ba016a227b5aee60d9ec340bb6659b2946b7
Emerson Vinicius <talk@evinicius.com>
2010-08-04 01:28:24 -0300

    Initial Commit + minimal Gemfile

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.rc'
+gem 'ruby-debug'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuestionBank::Application.config.secret_token = 'e5de2c4b110ed59c08ba8ecd576b65643f62a449b6a231ce93e2222303610bdb61d7acf8e693c41678b296bbce4d20bfe65f5b7eee18153fbf7994f509f75cac'
